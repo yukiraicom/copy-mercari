73a85dfb4e1ddb06cba747168b68d1e0eacb5939
yuki <36536316+yukiraicom@users.noreply.github.com>
2019-01-22 22:23:58 +0900

    new2アクション追加とルーティング

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    # super
+    # binding.pry
+  end
+
+  def new2
+    binding.pry
+  end
 
   # POST /resource
   # def create
@@ -56,7 +61,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    # super(resource)
+    users_sign_up2_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  }
+  devise_scope :user do
+    get 'users/sign_up2' => 'users/registrations#new2'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
