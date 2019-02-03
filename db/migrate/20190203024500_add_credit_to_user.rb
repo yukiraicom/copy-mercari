78155b1325b6175adb05b7001b1edf6a61b4d4dd
yukiraicom <yuukiarai19970524@gmail.com>
2019-02-03 12:14:48 +0900

    delete migration file

--- db/migrate/20190203024500_add_credit_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddCreditToUser < ActiveRecord::Migration[5.1]
-  def change
-    t.references :user, foreign_key: true
-    t.integer :card_number, null: false
-    t.integer :expiration_month, null: false
-    t.integer :expiration_year, null: false
-    t.integer :security_code, null: false
-  end
-end
