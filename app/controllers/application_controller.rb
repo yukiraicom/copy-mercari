aa9ad2de69dec462c97a6f71c2a80b5f896df54a
yukiraicom <yuukiarai19970524@gmail.com>
2019-02-04 22:44:58 +0900

    edit  devise_parameter_sanitizer.permit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,22 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kanai])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:prefecture])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:city])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:street])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:building_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:tel])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:card_number])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:expiration_month])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:expiration_year])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:security_coder])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, 
+    :profile, 
+    :first_name, 
+    :last_name, 
+    :first_name_kanai,
+    :last_name_kana,
+    :postal_code,
+    :prefecture,
+    :city,
+    :street,
+    :building_name,
+    :tel,
+    :card_number,
+    :expiration_month,
+    :expiration_year,
+    :security_coder
+  ])
   end
 end
