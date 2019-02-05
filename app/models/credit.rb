a420779ba2b9d6f099f2e5a0a98ed0ca2d0a7d64
yukiraicom <yuukiarai19970524@gmail.com>
2019-02-05 23:50:09 +0900

    誤字

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,5 +1,5 @@
 class Credit < ApplicationRecord
-  belongs_to: user
+  belongs_to :user
   validates :card_number, presence: true, uniqueness: true
   validates :expiration_month, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 12 }
   validates :expiration_year, presence: true, numericality: { only_integer: true }
