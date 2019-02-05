27c84eb3383bd3c0a22d9c8d7601ae9a2d0ecc32
yukiraicom <yuukiarai19970524@gmail.com>
2019-02-05 23:50:24 +0900

    user のemailをfaker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     nickname "test"
-    email "test@test"
+    sequence(:email) {Faker::Internet.email}
     password "000000"
     profile "this is test profile"
   end
