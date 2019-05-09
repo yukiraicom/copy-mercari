c850b60ba59f1b660d670d9f1bd48670014fedba
yukiraicom <yuukiarai19970524@gmail.com>
2019-05-09 20:02:23 +0900

    Edit migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190429103231) do
+ActiveRecord::Schema.define(version: 20190203031021) do
 
   create_table "adresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20190429103231) do
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
     t.string "profile", default: ""
+    t.string "provider"
+    t.string "uid"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -55,8 +57,6 @@ ActiveRecord::Schema.define(version: 20190429103231) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
