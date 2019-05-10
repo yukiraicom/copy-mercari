185d2074e06d399c38dcd6cd4cfac99f147b76f4
yukiraicom <yuukiarai19970524@gmail.com>
2019-05-10 22:16:51 +0900

    Add edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+  def edit
+    @user = current_user
+  end
+  def update
+    User.find(params[:id]).update(update_user)
+    redirect_to action: "edit"
+  end
+
+  private
+  def update_user
+    params.require(:user).permit(:nickname, :profile)
+  end
 end
