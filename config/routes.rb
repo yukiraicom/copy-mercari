aeb305cc0077d7c8d6c3706e1d07a816aab57788
yukiraicom <yuukiarai19970524@gmail.com>
2019-05-20 23:09:03 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     post 'users/sign_up3' => 'users/registrations#credit_create'
     get 'users/sign_up_fin' => 'users/registrations#fin'
   end
+  get 'users/logout', to: 'users#logout'
   resources :users, only: [:show, :edit, :update]
   resources :adresses, only: [:edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
