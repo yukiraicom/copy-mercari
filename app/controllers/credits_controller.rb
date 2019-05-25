5c662b03b1a08513bd90db1ab19bb0186fe1939f
yukiraicom <yuukiarai19970524@gmail.com>
2019-05-25 17:32:25 +0900

    create credit_controller

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,2 +1,21 @@
 class CreditsController < ApplicationController
+  def index
+  end
+  def new
+    @credit = Credit.new
+  end
+  def create
+    credit = Credit.new(credit_params)
+    if credit.save
+      redirect_to credits_path
+    else
+      @credit_error = credit.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def credit_params
+    params.require(:credit).permit(:card_number, :expiration_month, :expiration_year, :security_code).merge(user_id: current_user.id)
+  end
 end
