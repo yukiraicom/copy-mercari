f2642ac8de29452e72dbe4b19d180e9e56a33de2
yukiraicom <yuukiarai19970524@gmail.com>
2019-06-02 19:05:58 +0900

    add card_number

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,10 +1,15 @@
-class CreditsController < ApplicationController
+﻿class CreditsController < ApplicationController
   def index
+    if current_user.credit.present?
+      @credit_last_num = /\d{4}$/.match(current_user.credit.card_number.to_s)[0]
+      @expiration_date = "#{current_user.credit.expiration_month}" + "/" + "#{current_user.credit.expiration_year}"
+    end
   end
   def new
     @credit = Credit.new
   end
   def create
+    binding.pry
     credit = Credit.new(credit_params)
     if credit.save
       redirect_to credits_path
@@ -12,6 +17,10 @@ class CreditsController < ApplicationController
       @credit_error = credit.errors.full_messages
       render :new
     end
+  end
+  def destroy
+  Credit.find(params[:id]).destroy
+  redirect_to credits_path
   end
 
   private
