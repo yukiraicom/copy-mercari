791fad2675b97d9f1844a8928004c5d5f95e7a1a
yukiraicom <yuukiarai19970524@gmail.com>
2019-06-30 19:50:35 +0900

    render test

--- spec/controllers/tops_controller.rb
+++ spec/controllers/tops_controller.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe TopController do
   describe 'GET #index' do
-    get :index
-    expect(response).to render_template :index
+    it "render the :index template" do
+      get :index
+      expect(response).to render_template :index
+    end
   end
 end
