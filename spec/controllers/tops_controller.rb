694b85fe15606d284750c76162d89a17c0ad5bea
yukiraicom <yuukiarai19970524@gmail.com>
2019-07-11 18:04:47 +0900

    add test

--- spec/controllers/tops_controller.rb
+++ spec/controllers/tops_controller.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 describe TopController do
   describe 'GET #index' do
+    it "assigns the requested item to @item" do
+      items = create_list(:item,3)
+      get :index
+      expect(assigns(:items)).to match(items)
+    end
+
     it "render the :index template" do
       get :index
       expect(response).to render_template :index
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,5 +2,10 @@
 
 FactoryGirl.define do
   factory :item do
+    name {"sample1"}
+    condition 1
+    price 3000
+    description {"this is sample"}
+    status 1
   end
 end
