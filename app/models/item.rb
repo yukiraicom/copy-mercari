444653beb3f856ee2ca01afd6898d226af3aa215
yukiraicom <yuukiarai19970524@gmail.com>
2019-08-24 19:23:09 +0900

    fix nasted attributeを削除
    nastedしたものをpermitするとなんか消えてしまうため

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
     has_many :images
-    accepts_nested_attributes_for :images
 
     enum condition: {
       新品、未使用:1,未使用に近い:2,目立った傷や汚れなし:3,やや傷や汚れあり:4,傷や汚れあり:5,全体的に状態が悪い:6
