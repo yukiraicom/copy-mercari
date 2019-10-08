d6f063173d40e561c50a69fecf664afc537e20a7
yuki-ar <yuukiarai19970524@gmail.com>
2019-10-08 18:37:34 +0900

    サムネ生成とinputから削除する機能

--- app/assets/javascripts/exhibit.js
+++ app/assets/javascripts/exhibit.js
@@ -1,5 +1,5 @@
 $(function() {
-  ArrayuploadedImages = []; //formに入れたれた画像
+  ArrayuploadedImages = []; 
 
   function putCategoryOption(category) {
     var optionHtml = ""
@@ -23,7 +23,7 @@ $(function() {
     return html
   }
 
-  $(document).on("change", ".item-form__select-box#item_category_id", function(){
+  $(document).on("change", ".item-form__select-box#item_category_id", function () {
     var thisSelecter = $(this)
     selected_number = thisSelecter.val()
     var itemFormDev = $(".item-form__dev#categoty")
@@ -37,25 +37,23 @@ $(function() {
     $.ajax({
       type: "GET",
       url: '/api/item',
-      data: {selected_number: selected_number}
+      data: { selected_number: selected_number }
     })
-      .done(function(category){
+      .done(function (category) {
         var previousId = 0
-        previousOptions.each(function(i, option){
-          if($(option).data("parent-id") == 0 || $(option).data("parent-id") == previousId ){
+        previousOptions.each(function (i, option) {
+          if ($(option).data("parent-id") == 0 || $(option).data("parent-id") == previousId) {
             itemFormGroup.append($(option))
             previousId = $(option).find('.item-form__select-box#item_category_id').val()
           }
         })
-        if(category.length != 0) itemFormGroup.append(buildCategory(category))
+        if (category.length != 0) itemFormGroup.append(buildCategory(category))
       })
   })
 })
 
-
 //以下 画像サムネ生成
 $(function() {
-  // $('dropbox--container__dropbox__file').after('<span></span>');
   function buildImg(imgSrc, lastModified) {
     var html = `
       <li>
@@ -69,32 +67,17 @@ $(function() {
   }
 
   $('.dropbox--container__dropbox__file').change(function() {
-    // $('span').html('');
     var file = $(this).prop('files');
-
     var img_count = 1;
     $(file).each(function(i) {
-      if (img_count > 10) {
-        return false;
-      }
-
       if (! file[i].type.match('image.*')) {
-        $(this).val('');
-        $('span').html('');
         return;
       }
-
       var reader = new FileReader();
       reader.onload = function() {
-        // $('.dropbox--container__items ul li').append(img_src).append(editOrDeleteButton);
         $('.dropbox--container__items ul').append(buildImg(reader.result, file[i].lastModified))
       }
       reader.readAsDataURL(file[i]);
-      // console.log(file[i].lastModified)
-        //サムネ削除のためのformdata生成
-      // if (typeof ArrayuploadedImages === 'undefined' || ArrayuploadedImages.length === 0 ){
-      //   ArrayuploadedImages = Array.from($(this).parents(".dropbox--container").find(".dropbox--container__dropbox__file")[0].files);
-      // }   
       ArrayuploadedImages.push(file[i])
       img_count = img_count + 1;
     });
@@ -105,28 +88,45 @@ $(function() {
 $(function() {
   $(document).on("click", ".dropbox--container__items__edit a", function(e){
     e.preventDefault();
-    var imageOfInput = $(this).parents(".dropbox--container").find(".dropbox--container__dropbox__file")
     var clickedImageLastModified = $(this).parents().data("lastmodified");
-
     for(var i in ArrayuploadedImages){
       if(ArrayuploadedImages[i].lastModified === clickedImageLastModified){
         ArrayuploadedImages.splice(i, 1)
       }
     }
+    //サムネを消す
+    $(this).parents(".dropbox--container__items ul li").remove();
+  })
+})
 
-    var formData = new FormData($(this).parents(".inner__item-form")[0]); //画像だけ空のformdatam
-
-  
+$(function(){
+  $("form").on("submit", function(e){
+    e.preventDefault();
+    if(ArrayuploadedImages.length === 0){
+      alert("画像は必須です。")
+      exit
+    }
+    var formData = new FormData($(this)[0]);
     formData.delete("item[image][image][]")
-
-    
     for(var i in ArrayuploadedImages){
-      console.log(ArrayuploadedImages[i])
       formData.append("item[image][image][]", ArrayuploadedImages[i])
     }
 
-
-    //サムネを消す
-    $(this).parents(".dropbox--container__items ul li").remove();
+    $.ajax({
+      type: "POST",
+      url: "/items",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(status){
+      if(status.status === "ok"){
+        window.location.href = "/"
+      }
+    })
+    .fail(function(){
+      $("form").unbind('submit').submit()
+    })
   })
 })
