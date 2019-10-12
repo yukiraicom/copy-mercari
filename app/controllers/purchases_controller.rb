76565b9fccdbf6ae3b3afcdb4fa88de2f934e018
yuki-ar <yuukiarai19970524@gmail.com>
2019-10-13 03:33:17 +0900

    add エラーが起きた場合の処理

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,12 +5,26 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    customer_id = current_user.credit.customer_id
-    charge = Payjp::Charge.create(
-    amount: params[:item_price],
-    currency: 'jpy',
-    customer: customer_id,
-    )
-    redirect_to root_path
+    if purchaseItem
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :new
+    end
+  end
+
+  private
+  def purchaseItem
+    begin
+      customer_id = current_user.credit.customer_id
+      charge = Payjp::Charge.create(
+      amount: params[:item_price],
+      currency: 'jpy',
+      customer: customer_id,
+      )
+      Item.find(params[:item_id]).update(status: 4)
+    rescue
+      false
+    end
   end
 end
