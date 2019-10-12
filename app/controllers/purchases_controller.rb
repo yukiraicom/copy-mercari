1451a3957eb3753d3fb41bea9d8937030ac72dbd
yuki-ar <yuukiarai19970524@gmail.com>
2019-10-13 03:01:27 +0900

    fix 購入画面の情報をdbから取得

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,16 @@
 class PurchasesController < ApplicationController
+  
   def new
+    @item = Item.find(params[:id])
+  end
+
+  def create
+    customer_id = current_user.card.customer_id
+    charge = Payjp::Charge.create(
+    amount: 3500,
+    currency: 'jpy',
+    customer: customer_id,
+    )
+    redirect_to "/products"
   end
 end
