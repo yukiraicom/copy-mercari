53e828655fafbf3c3f1c9dfe78830b3bd4bff6c8
yuki-ar <yuukiarai19970524@gmail.com>
2019-10-14 01:04:12 +0900

    add 詳細検索のカテゴリー表示

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,48 @@
+$(function () {
+  function putCategoryOption(category) {
+      var optionHtml = ""
+      category.forEach(function (e) {
+        optionHtml += `<option value='${e.id}'>${e.name}</option>\n`
+      })
+      return optionHtml
+    }
+    function buildCategory(category) {
+      var html = `
+        <select class="item-form__select-box" name="item[category_id]" id="search_category_id" data-parent-id="${category[0].parent_id}">
+          <option value="">---</option>
+          ${putCategoryOption(category)}
+        </select>
+        <i class="fa fa-chevron-down item-form__select-icon"></i>
+      `
+      return html
+    }
+
+  $(document).on("change", ".item-form__select-box#search_category_id", function () {
+    var thisSelecter = $(this)
+    selected_number = thisSelecter.val()
+    var detailSelect = thisSelecter.parents(".detailSelect")
+    var previousOptions = $(this).parents(".detailSelect").find(".item-form__select-box#search_category_id")
+    detailSelect.empty()
+    $.ajax({
+      type: "GET",
+      url: '/api/item',
+      data: { selected_number: selected_number }
+    })
+      .done(function (category) {
+        var previousId = 0
+        for (preselecter of previousOptions) {
+          previousId = $(preselecter).val();
+          if (category.length == 0) {
+            detailSelect.append(preselecter)
+          } else if (previousId == category[0].parent_id) {
+            detailSelect.append(preselecter)
+          } else if ($(preselecter).data("parent-id") == 0) {
+            detailSelect.append(preselecter)
+          }
+        }
+        if (category.length != 0) {
+          detailSelect.append(buildCategory(category))
+        }
+      })
+  })
+})
