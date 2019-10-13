436c596e8059633949431743ae4d51f9f7ce61eb
yuki-ar <yuukiarai19970524@gmail.com>
2019-10-13 19:24:55 +0900

    fix コントローラーでformを分岐

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   
+  def index
+    @q = Item.ransack(params[:q])
+  end
+  
   def create
     @q = Item.ransack(params[:q])
     @item = @q.result
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,5 @@
 class TopController < ApplicationController
   def index
     @items = Item.all
-    @q = Item.ransack(params[:q])
   end
 end
