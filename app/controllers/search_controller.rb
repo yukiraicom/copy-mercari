6a6465f647832410b118e3487e5ae2b948872164
yuki-ar <yuukiarai19970524@gmail.com>
2019-10-13 23:18:03 +0900

    add 詳細検索のhtmlcss

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,14 @@ class SearchController < ApplicationController
   def index
     @q = Item.ransack(params[:q])
   end
-  
+
   def create
     @q = Item.ransack(params[:q])
     @item = @q.result
+    @conditions = Item.conditions.keys
+    @category = Category.where(parent_id: 0)
+    @postages = Item.postages.keys
+    @status = Item.statuses.keys
     render :index
   end
 end
