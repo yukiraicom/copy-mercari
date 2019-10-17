8ac1c61a9f99e28ad9e68917a4ceece6eba73298
yuki-ar <yuukiarai19970524@gmail.com>
2019-10-17 15:58:04 +0900

    add basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth, if: :production?
   require 'payjp'
   Payjp::api_key = ENV['testPayjpKey']
 
@@ -27,5 +28,15 @@ class ApplicationController < ActionController::Base
         :security_coder
       ]
     )
+  end
+  
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,3 @@
 server '3.115.86.131', user: 'ec2-user', roles: %w{app db web}
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
