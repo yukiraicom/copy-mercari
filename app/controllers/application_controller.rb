eae01cacd55b8fffed79e6596cbba1e1f81b4d10
yuki-ar <yuukiarai19970524@gmail.com>
2019-10-18 18:21:19 +0900

    fix sign_inしててadressがない場合リダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
+  before_action :checkUserAdress
   require 'payjp'
   Payjp::api_key = ENV['testPayjpKey']
 
@@ -37,6 +38,13 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
+  def checkUserAdress
+    if user_signed_in? && current_user.adress.nil? && controller_name != "registrations" && controller_name != "sessions"
+      flash[:notice] = "サインアップが完了しました。住所を入力してください。"
+      redirect_to users_sign_up2_path
     end
   end
 end
